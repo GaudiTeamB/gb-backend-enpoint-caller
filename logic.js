f4a50a38239b7331ebbb7ce1d6c6416038710de2
esantonja <enrique.santonja@travelport.com>
2017-03-17 17:06:33 +0100

    updated logic

--- logic.js
+++ logic.js
@@ -2,69 +2,99 @@ var urlValidations = require('./urlValidations.js');
 
 module.exports = {
 
-retrieveUrl: function(request){
-    var url = require('url');
-    var destinationHost = url.parse(request.url, true).query.url;
-    return destinationHost;
-},
+    retrieveUrl: function (request) {
+        var url = require('url');
+        var destinationHost = url.parse(request.url, true).query.url;
+        return destinationHost;
+    },
 
-retrieveNumberOfCalls: function(request){
-    var url = require('url');
-    var temp = url.parse(request.url, true).query.times;
+    retrieveNumberOfCalls: function (request) {
+        var url = require('url');
+        var temp = url.parse(request.url, true).query.times;
 
-    var numberOfCalls = 1;
-    if(!isNaN(temp))
-    {
-        numberOfCalls = Number(temp);
-    }
+        var numberOfCalls = 1;
+        if (!isNaN(temp)) {
+            numberOfCalls = Number(temp);
+        }
 
-    return numberOfCalls;
-},
+        return numberOfCalls;
+    },
 
-isValidUrl: function (destinationUrl) {
-    var url = require('url');
-    var urlParsed = url.parse(destinationUrl, true);
+    isValidUrl: function (destinationUrl) {
+        var url = require('url');
+        var urlParsed = url.parse(destinationUrl, true);
 
-    // TODO: isValidHost is not the proper check for this field. Custom check needed.
-    var isValid = (urlParsed.href && urlValidations.isValidHost(urlParsed.href)) || 
-                  (urlValidations.isValidHost(urlParsed.host) &&
-                  urlValidations.isValidProtocol(urlParsed.protocol));
+        // TODO: isValidHost is not the proper check for this field. Custom check needed.
+        var isValid = (urlParsed.href && urlValidations.isValidHost(urlParsed.href)) ||
+            (urlValidations.isValidHost(urlParsed.host) &&
+                urlValidations.isValidProtocol(urlParsed.protocol));
 
-     return isValid;
-},
+        return isValid;
+    },
 
 
-httpCall: function(destinationHost, callId, callNumber) {
-    var urlPath = "/?url=" + destinationHost + 
-                  "&id=" + callId + 
-                  "&number=" + callNumber;
-                  
-    return new Promise(function (resolve, reject) {
-        var http = require('http');
-        var options = {
-                    host: 'localhost',
-                    port: 1980,
-                    path: urlPath
-                };
+    httpCall: function (destinationHost, callId, callNumber) {
+        var urlPath = "/?url=" + destinationHost +
+            "&id=" + callId +
+            "&number=" + callNumber;
 
-        http.get(options, function(res) {
-            res.on("data", function() {
-                resolve(res);
+        return new Promise(function (resolve, reject) {
+            var http = require('http');
+            var options = {
+                host: 'localhost',
+                port: 1980,
+                path: urlPath
+            };
+
+            http.get(options, function (res) {
+                res.on("data", function () {
+                    resolve(res);
+                });
+            }).on('error', function (e) {
+                reject(e.message);
             });
-        }).on('error', function(e) {
-            reject(e.message);
         });
-    });
-},
+    },
 
-sendResponse: function(response, result, message){
-    if(result === 200){
-        response.writeHead(200, {"Content-Type": "application/json"});
-        response.end(JSON.stringify({ message : message }));
-    } else {
-        response.writeHead(500, {"Content-Type": "application/json"});
-        response.end(JSON.stringify({ message : message }));
-    }
-},
+    retrieveData: function (id) {
+        return new Promise(function(resolve, reject) {
+        var mongoClient = require("mongodb").MongoClient;
+
+        mongoClient.connect("mongodb://gb-mongo:WXFU43PvnFcyHV3GW6ZmJvYhr613ZBbyuhYA3azpvpUiqDJEmHxMIXWwd4XQMPcWRzMKMy8S5zhZHz0GihVXuw==@gb-mongo.documents.azure.com:10250/?ssl=true",
+            function (err, db) {
+                db.collection('gb-respose-times').find({guid: id}).toArray(function(err, result){
+                    var endpoints = {};
+                    var emeaResults = result.map(value => {
+                        if(value.endpoint in endpoints){
+                            endpoints[value.endpoint] += value.lapse;
+                        } else {
+                            endpoints[value.endpoint] = value.lapse;
+                        }
+                    });
+
+                    var averageResult = [
+                        {
+                            endpoint: "EMEA",
+                            average: endpoints.EMEA / result.length,
+                        }
+                    ];
+                    resolve(averageResult);
+                });
+            });
+        });
+
+    },
+
+    sendResponse: function (response, result, message, id) {
+        if (result === 200) {
+            this.retrieveData(id).then(function(result){
+                response.writeHead(200, { "Content-Type": "application/json" })
+                response.end(JSON.stringify({ data: result }));
+            });
+        } else {
+            response.writeHead(500, { "Content-Type": "application/json" });
+            response.end(JSON.stringify({ error: message }));
+        }
+    },
 
 }
--- server.js
+++ server.js
@@ -5,34 +5,36 @@ var uuid = require('react-native-uuid');
 
 var app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}); 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (request, response) {
-    
-   var destinationHost = logic.retrieveUrl(request);
-   var numberOfCalls = logic.retrieveNumberOfCalls(request);
-   var callId = uuid.v4();
 
-    console.log("destinationHost: " + destinationHost + " numberOfCalls="+ numberOfCalls  + " callId=" + callId);
+    var destinationHost = logic.retrieveUrl(request);
+    var numberOfCalls = logic.retrieveNumberOfCalls(request);
+    var callId = uuid.v4();
 
-    if(destinationHost === undefined || !logic.isValidUrl(destinationHost || numberOfCalls < 1)) {
+    console.log("destinationHost: " + destinationHost + " numberOfCalls=" + numberOfCalls + " callId=" + callId);
+
+    if (destinationHost === undefined || !logic.isValidUrl(destinationHost || numberOfCalls < 1)) {
         logic.sendResponse(response, 404, "Invalid URL: " + destinationHost);
     }
     else {
-        for(var i = 0; i < numberOfCalls; i++)
-        {
+        var finishedTimes = numberOfCalls;
+        for (var i = 0; i < numberOfCalls; i++) {
             logic.httpCall(destinationHost, callId, i)
-                .then(function (resp){
-                    if( i === (numberOfCalls - 1))
-                    {
-                        logic.sendResponse(resp, 200, "Ok");
+                .then(function (resp) {
+                    finishedTimes--;
+                    console.log("Number of calls " + finishedTimes + "/" + numberOfCalls + "finished OK");
+                    if (finishedTimes === 0) {
+                        logic.sendResponse(response, 200, "Ok", callId);
                     }
                 }).catch(function (error) {
-                    logic.sendResponse(response, 500, "Error: "+ error);
+                    console.log("Call " + i + " finished ERROR: " + error, callId);
+                    logic.sendResponse(response, 500, "Error: " + error);
                 });
         }
     }
@@ -40,8 +42,8 @@ app.get('/', function (request, response) {
 
 // POST method route
 app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-  console.log('POST method called');
+    res.send('POST request to the homepage')
+    console.log('POST method called');
 });
 
 app.listen(port);
