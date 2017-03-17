329b07286b49b644002e87dffc74da6c5b13dbc0
esantonja <enrique.santonja@travelport.com>
2017-03-17 11:49:44 +0100

    Multi calls implemented

--- server.js
+++ server.js
@@ -15,19 +15,24 @@ app.get('/', function (request, response) {
     
    var destinationHost = logic.retrieveUrl(request);
    var numberOfCalls = logic.retrieveNumberOfCalls(request);
+   var callId = uuid.v4();
 
     if(destinationHost === undefined || !logic.isValidUrl(destinationHost || numberOfCalls < 1)) {
         logic.sendErrorResponse(response, 404, "Invalid URL: " + destinationHost)
     }
     else {
-        var callId= uuid.v4();
-
         for(var i = 0; i < numberOfCalls; i++)
         {
-            logic.httpCall(detinationHost, callId);
+            logic.httpCall(detinationHost, callId, i)
+                .then(function (res){
+                    if( i === (numberOfCalls - 1))
+                    {
+                        logic.sendResponse(response, 200, "Ok")
+                    }
+                }).catch(function (error) {
+                    logic.sendResponse(error, 500, "Error");
+                });
         }
-
-        logic.sendResponse(response, 200, "Ok")
     }
 });
 
